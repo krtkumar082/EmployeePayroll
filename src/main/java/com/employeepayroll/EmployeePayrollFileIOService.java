011214c6d2a6e7d243b9cf062a08e431a998242f
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-11-05 20:07:59 +0530

    [Kirti]Added:update the salary with name and created prepared statement also

--- src/main/java/com/employeepayroll/EmployeePayrollFileIOService.java
+++ src/main/java/com/employeepayroll/EmployeePayrollFileIOService.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.employeepayroll.EmpPayrollData;
 import com.employeepayroll.EmployeePayrollService.IOService;
 
 
--- src/main/java/com/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.empdb.EmployeePayrollException;
+import com.empdb.EmployeePayrollFileDBService;
 import com.employeepayroll.EmployeePayrollService.IOService;
+
+
 
 public class EmployeePayrollService {
    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO};
    private List<EmpPayrollData> employeePayrollList;
-   
-   
-   EmployeePayrollService(){
-	   
-   }
+   private EmployeePayrollFileDBService employeePayrollDBService;
+	
+	public EmployeePayrollService() {
+		employeePayrollDBService = EmployeePayrollFileDBService.getInstance();
+	}
    
    public EmployeePayrollService(List<EmpPayrollData> employeePayrollList){
+	   this();
 	   this.employeePayrollList=employeePayrollList;
    }
    
@@ -57,6 +62,34 @@ public List<EmpPayrollData> readEmployeePayrollData(IOService ioService) {
 	return employePayrollDataList;
 }
 
+public List<EmpPayrollData> readEmpPayrollData(IOService ioService) throws EmployeePayrollException {
+	if(ioService.equals(IOService.DB_IO))
+		this.employeePayrollList=employeePayrollDBService.readData();
+	return employeePayrollList;
+}
+
+public boolean checkEmployeePayrollInSyncWithDB(String name) {
+	List<EmpPayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+	return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
+}
+
+
+public void updateEmployeeSalary(String name, double salary) {
+	int result = employeePayrollDBService.updateEmployeeData(name, salary);
+	if (result == 0)
+		return;
+	EmpPayrollData employeePayrollData = this.getEmployeePayrollData(name);
+	if (employeePayrollData != null)
+		employeePayrollData.salary = salary;
+	
+}
+
+private EmpPayrollData getEmployeePayrollData(String name) {
+	return this.employeePayrollList.stream()
+			.filter(employeePayrollData -> employeePayrollData.getName().equals(name))
+			.findFirst()
+			.orElse(null);
+}
 
 public void printData(IOService ioService) {
 	if(ioService.equals(IOService.FILE_IO))
@@ -69,4 +102,9 @@ public long countEntries(IOService ioService) {
 		return new EmployeePayrollFileIOService().countEntries();
 	return 0;
 }
+
+
+
+
+
 }
